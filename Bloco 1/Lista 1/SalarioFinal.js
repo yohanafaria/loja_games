8c76d37bbec9f90a98779935ae261d335b5bfffd
yohanafaria <145880332+yohanafaria@users.noreply.github.com>
2024-08-08 02:29:34 -0300

    RM

--- Bloco 1/Lista 1/SalarioFinal.js
+++
@@ -1,8 +0,0 @@
-// Calcula o valor final do salário, com o abono
-
-const leia = require('readline-sync');
-let bruto;
-let abono;
-bruto = leia.questionFloat("\nPor favor, informe seu salário:");
-abono = leia.questionFloat("Agora, insira o valor do seu abono: "); 
-console.log("O valor do seu salário será: " + (bruto+abono))
